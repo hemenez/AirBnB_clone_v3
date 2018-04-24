3b378c16fc077f712bf9911e825e55aec3d78eb2
Lindsey Hemenez <lindseyhemenez@gmail.com>
2018-04-24 20:11:02 +0000

    Added imports for amenities and users

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 from flask import Blueprint
 
 
@@ -5,3 +6,5 @@ app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
 
 from api.v1.views.index import *
+from api.v1.views.amenities import *
+from api.v1.views.users import *
