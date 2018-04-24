f6ffdd1a9b869cb4bbaff46df530b0c860a0e4b9
Lindsey Hemenez <lindseyhemenez@gmail.com>
2018-04-24 00:26:07 +0000

    Added error handler to return JSON formatted 404 code

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-from flask import Flask
+from flask import Flask, make_response, jsonify
 from models import storage
 from os import getenv
 from api.v1.views import app_views
@@ -13,6 +13,10 @@ app.register_blueprint(app_views)
 @app.teardown_appcontext
 def close_method(exception):
     storage.close()
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({"error": "Not found"}), 404)
 
 if __name__ == "__main__":
     app_host = getenv('HBNB_API_HOST')
