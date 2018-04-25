6e652107bd793286b13696193545a47333b5c97f
Lindsey Hemenez <lindseyhemenez@gmail.com>
2018-04-25 22:45:04 +0000

    added docstring

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+'''
+status module
+'''
 from models import storage
 from api.v1.views import app_views
 from flask import jsonify
@@ -6,10 +9,16 @@ from flask import jsonify
 
 @app_views.route('/status')
 def response_code():
+    '''
+    function returns a status response
+    '''
     return jsonify({"status": "OK"})
 
 @app_views.route('/stats')
 def count_objects():
+    '''
+    function counts how m any objects are in the database
+    '''
     count_dict = {}
     count_dict["amenities"] = storage.count("Amenity")
     count_dict["cities"] = storage.count("City")
